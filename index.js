7d698dfd087a5fd7a5038176e223afac9270dcc6
Мирошников Никита <plokijuh2112@gmail.com>
2024-09-15 10:19:12 +0300

    End my bot

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
-const { Bot, GrammyError, HttpError, InlineKeyboard } = require('grammy');
+const { Bot, GrammyError, HttpError, InlineKeyboard, InputFile } = require('grammy');
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
 const chanelID = process.env.CHANNEL_ID;
 
-const documentID = process.env.DOC_ID;
-
-
+// прописываем доступные команды в окне menu
 bot.api.setMyCommands([
     { command: 'start', description: 'Начало работы' },
 ]);
 
+// команда start
 bot.command('start', async (ctx) => {
     const кeyboard = new InlineKeyboard()
         .text('Проверить подписку', 'check');
-    await ctx.reply('Здравствуйте, подишитесь на наш канал и получите файл "Как увеличить доход при помощи дизайна."', { reply_markup: кeyboard });
+    await ctx.reply('Здравствуйте, подишитесь на наш канал {канал} и получите файл "Как увеличить доход при помощи дизайна."', { reply_markup: кeyboard });
 });
 
+// после команды start и вызова инлайн-клавиатуры прописываем команду check с помощью callbackQuery, которая используется в связке с InlineKeyboard
 bot.callbackQuery('check', async (ctx) => {
     const userID = ctx.from.id;
     await ctx.answerCallbackQuery();
@@ -25,18 +25,19 @@ bot.callbackQuery('check', async (ctx) => {
     try {
         //  Вытаскиваем инфу о подписке
         const chatMember = await ctx.api.getChatMember(chanelID, userID);
-
+        // Проверяем подписку
         if (chatMember.status === 'member' || chatMember.status === 'creator' || chatMember.status === 'administrator') {
-            
-            await ctx.reply("Спасибо за вашу подписку, вот ваш файл ");
-    
-            await ctx.replyWithDocument(documentID);
-
+        
+        // Отправляем файл
+            await ctx.reply("Спасибо за вашу подписку, вот ваш файл: ");
+        
+            await bot.api.sendDocument(ctx.chat.id, new InputFile('путь к файлу'));
+              
         } else {
             await ctx.reply("Извините, но вы не подписаны на канал. Подпишитесь и попробуйте еще раз:)");
         }
     } catch (error) {
-        await ctx.reply(' Сейчас невозможно проверить подписку, попробйуйте позже.');
+        await ctx.reply(' Сейчас невозможно проверить подписку, попробуйте позже.');
         console.error('Ошибка:', error.response?.description || error);
     }
 });
